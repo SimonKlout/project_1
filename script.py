485e453497e22782b1ee8643fd627e9e7fe8e216
SimonKlout <70890716+SimonKlout@users.noreply.github.com>
2024-04-16 10:20:46 +0200

    Date change

--- script.py
+++ script.py
@@ -1,5 +1,4 @@
 from dateutil import parser
 
-
-parsed_date = parser.parse('3rd January 2014')
+parsed_date = parser.parse('16th April 2024')
 print('The type of the parsed date is {}'.format(type(parsed_date)))
